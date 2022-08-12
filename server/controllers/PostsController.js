4cb7ceabda2db0cf27b6f6ea4ad949378fc52d0c
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-12 11:59:58 -0600

    Creates and Posts work

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -2,33 +2,33 @@ import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 import { postsService } from "../services/PostsService.js";
 
-export class PostsController extends BaseController{
-    constructor(){
-        super('/api/posts')
-        this.router
-        .get('',this.getAll)
-        .use(Auth0Provider.getAuthorizedUserInfo)
-        .post('',this.createPost)
-    }
-    
-    
-    async getAll(req,res,next){
-        try {
-            const query = req.query
-            const posts = await postsService.getAll(query)
-            return res.send(posts)   
-        } catch (error) {
-            next(error)
-            
-        }
-    }
-    async createPost(req,res,next) {
-        try {
-        throw new console.error();    
-        } catch (error) {
-        next(error)    
-        }
-    }
+export class PostsController extends BaseController {
+  constructor() {
+    super("/api/posts");
+    this.router
+      .get("", this.getAll)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post("", this.createPost)
+  }
 
-    // NOTE COMMENT HERE 
+  async getAll(req, res, next) {
+    try {
+      const query = req.query;
+      const posts = await postsService.getAll(query);
+      return res.send(posts);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async createPost(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id;
+      const post = await await postsService.createPost(req.body);
+      res.send(post)
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // NOTE COMMENT HERE
 }
--- server/models/Post.js
+++ server/models/Post.js
@@ -3,14 +3,13 @@ const Schema = mongoose.Schema
 
 export const PostSchema = new Schema(
     {
-        team: {type: String, required: true, minlength: 5},
+        team: {type: String, required: true, minlength: 4},
         creatorId: {type: Schema.Types.ObjectId, required: true, ref: 'Account'},
         title: {type: String, required: true, minlength: 5, maxlength: 20},
         img: {type: String, required: true},
         description: {type: String, required: true, minlength: 10, maxlength: 200},
         date: {type: String, required: true,},
         votes: {type: Number, required: true, default: 0},
-        blank: {type: String,}
     },
     { timestamps: true, toJSON: { virtuals: true } }
 )
