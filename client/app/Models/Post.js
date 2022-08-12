2c2d08e4e3b94e8e645f4e9a04afaccf999894ec
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-08-12 15:25:35 -0600

    updates

--- client/app/Models/Post.js
+++ client/app/Models/Post.js
@@ -8,6 +8,7 @@ export class Post {
     this.description = data.description
     this.votes = data.votes
     this.date = data.date
+    this.id = data.id
 
   }
 
@@ -26,7 +27,7 @@ export class Post {
                 </span>
                 <span>
                   <i class="mdi mdi-pencil text-info selectable me-2" title="Edit Post"></i>
-                  <i class="mdi mdi-delete text-danger selectable" title="Delete Post" onclick="app.postsController.deletePost('${this.creatorId}')"></i>
+                  <i class="mdi mdi-delete text-danger selectable" title="Delete Post" onclick="app.postsController.deletePost('${this.id}')"></i>
                 </span>
               </div>
               <img src="${this.img}" class="card-img px-5" alt="...">
