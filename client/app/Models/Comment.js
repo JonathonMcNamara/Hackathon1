481a3e808d91edca23e42f2c450747b9fa20aff0
Andrew LaRue <pikey6957@gmail.com>
2022-08-12 18:55:23 -0600

    andrew

--- client/app/Models/Comment.js
+++ client/app/Models/Comment.js
@@ -9,15 +9,19 @@ export class Comment {
 
   get commentTemplate() {
     return `
-    <h5>${this.creator.name}</h5>
+    <div class="bg-light my-2 p-2 text-dark rounded">
+    <div class="d-flex flex-row justify-content-between">
+    <h6>${this.creator.name}</h6>
     <span>
       <i class="mdi mdi-pencil text-info selectable me-2" title="Edit Comment"></i>
       <i class="mdi mdi-delete text-danger selectable" title="Delete Comment"></i>
-    </span>
+      </span>
+    </div>
     <div>
       <p>
         ${this.description}
       </p>
+    </div>
     </div>
     `
   }
